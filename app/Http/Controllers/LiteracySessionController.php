a4d3d5c7948639b4fee2d936602586a75c9e165b
kashif <syedraz1212@gmail.com>
2024-05-14 16:02:25 +0500

    sorted chart image error

--- app/Http/Controllers/LiteracySessionController.php
+++ app/Http/Controllers/LiteracySessionController.php
@@ -246,10 +246,9 @@ class LiteracySessionController extends Controller
     {
         $session_id = $request->session_id;
         $image = $request->input('session_chart_base64'); // image base64 encoded
-        preg_match("/data:image\/(.*?);/", $image, $image_extension); // extract the image extension
         $image = preg_replace('/data:image\/(.*?);base64,/', '', $image); // remove the type part
         $image = str_replace(' ', '+', $image);
-        $imageName = 'image_' . time() . '.' . $image_extension[1]; //generating unique file name;
+        $imageName = 'image_' . time() . '.' ."png"; //generating unique file name;
         $file = base64_decode($image);
         $success = file_put_contents(public_path().'/charts/'.$imageName, $file);
         $literacySession = LiteracySession::find($session_id);
